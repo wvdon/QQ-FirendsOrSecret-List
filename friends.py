a6e018313cdc871e7fe0289aac3d7485e6ec0b14
wvdon <mail@wvdon.com>
2020-03-16 16:38:23 +0800

    first

---
+++ friends.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+import time
+browser = webdriver.Chrome(executable_path='C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe')
+print("请你确保你的QQ已在电脑登录，当浏览器跳出快速登陆的时候选择你需要登录的QQ,每次只能爬取一个qq")
+#inp = input("您的选择是：")
+qq_id = input("输入你的qq账户：")
+#qq_pass = input("输入你的qq密码：")
+browser.get("https://qzone.qq.com/")
+browser.maximize_window()
+time.sleep(10)
+browser.get("https://user.qzone.qq.com/"+str(qq_id)+"/profile/permit")
+time.sleep(5)
+browser.switch_to.frame("ttinfo")
+browser.find_element_by_id('entry_desc').click()
+time.sleep(2)
+for i in range(20):
+    browser.execute_script('document.getElementsByClassName("friends-detail friends-detail-visit qz-scrollbar")[0].scrollTop=10000')
+    time.sleep(1)
+for link in browser.find_elements_by_xpath("//*[@data-uin]"):
+    #print (link.get_attribute('data-uin'))
+    with open(str(qq_id)+'.txt','a') as f:
+        f.write(link.get_attribute('data-uin')+'\n')
+        f.close()
+print("okk")
---
+++ locater.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+
+l=input("输入有为是好友秘密的QQ账号，多个请用空格隔开：")
+yes_list = l.split(" ")
+ms=input("输入有为非好友秘密的QQ账号，多个请用空格隔开：")
+no_list = ms.split(" ")
+df = pd.DataFrame(columns=["qq"])
+count =0
+if yes_list:
+    for i in yes_list:
+        a = []
+        f = open(i+".txt").read().split("\n")
+        for j in f:
+            a.append(j)
+        d=pd.DataFrame(np.array(a),columns=["qq"])
+        if (count == 0):
+            df = df.append(d)
+            count=count+1
+        else:
+            df = pd.merge(df,d,how='inner')
+if no_list:
+    for i in no_list:
+        b = []
+        f = open(i + ".txt").read().split("\n")
+        for j in f:
+            b.append(j)
+        d = pd.DataFrame(np.array(b), columns=["qq"])
+        df = df.append(d)
+        df = df.append(d)
+        df = df.drop_duplicates(subset=["qq"], keep=False)
+        # a与b的差集 a=a-b
+print("筛选结果如下:")
+for i in range(0, len(df)):
+    print(df.iloc[i]['qq'])
